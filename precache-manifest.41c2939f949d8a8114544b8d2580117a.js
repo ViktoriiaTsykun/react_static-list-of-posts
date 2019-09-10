ae82937f79f8a25c2faeea9ae9e7984d658bba73
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-11 01:07:56 +0300

    Updates

---
+++ precache-manifest.41c2939f949d8a8114544b8d2580117a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324c2053b6112ab0b2a047086d447d46",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "c0896f90aa4f01bd2ebf",
+    "url": "/react_static-list-of-posts/static/css/main.e8034749.chunk.css"
+  },
+  {
+    "revision": "ca0029b3c2516f928e0d",
+    "url": "/react_static-list-of-posts/static/js/2.158af266.chunk.js"
+  },
+  {
+    "revision": "c0896f90aa4f01bd2ebf",
+    "url": "/react_static-list-of-posts/static/js/main.d3fda919.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
